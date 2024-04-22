e4a94bccda44db7797f198f77bfd3df0c7d3c61c
U Harshitha <harshitha.srikk@gmail.com>
2024-04-22 19:29:15 +0530

    equipment

--- client/src/screens/EquipmentForm.js
+++ client/src/screens/EquipmentForm.js
@@ -1,40 +1,52 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function EquipmentForm() {
-  let [err, setErr] = useState('')
-  let { register, handleSubmit } = useForm()
-  let navigate = useNavigate()
+  let [err, setErr] = useState('');
+  let { register, handleSubmit } = useForm();
+  let navigate = useNavigate();
+
   async function handleFormSubmit() {
-    let res = await axios.post("http://localhost:4000/user-api/toolsubmit")
+    let res = await axios.post("http://localhost:4000/user-api/toolsubmit");
     if (res.data.message === 'Successful')
-      navigate('/')
+      navigate('/');
     else
-      setErr(res.data.message)
+      setErr(res.data.message);
   }
+
   return (
     <form className='container mt-3' onSubmit={handleSubmit(handleFormSubmit)}>
-      {err}
-      <h1 className='text-center'>Booking Form</h1>
-      <div className='mx-auto'>
-        <label className='form-label'>Name</label>
-        <input type='text' className='form-control w-75 mx-auto' {...register('name', { required: true })} />
-        <label className='form-label align-center'>Email</label>
-        <input type='email' className='form-control w-75 mx-auto' {...register('email', { required: true })} />
-        <label className='form-label'>Phone number</label>
-        <input type='text' className='form-control w-75 mx-auto' {...register('phone', { required: true, minLength: 10, maxLength: 10 })} />
-        <label className='form-label'>Tool name</label>
-        <input type='text' className='form-control w-75 mx-auto' {...register('toolname', { required: true })} />
-        <label className='form-label'>Tool Image</label>
-        <input type='file' accept="image/*" className='form-control w-75 mx-auto' {...register('toolimage', { required: true })} />
+      {err && <div className="alert alert-danger">{err}</div>}
+      <h1 className='text-center mb-4'>Booking Form</h1>
+      <div className='row justify-content-center'>
+        <div className='col-md-6'>
+          <label className='form-label'>Name</label>
+          <input type='text' className='form-control' {...register('name', { required: true })} />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Email</label>
+          <input type='email' className='form-control' {...register('email', { required: true })} />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Phone number</label>
+          <input type='text' className='form-control' {...register('phone', { required: true, minLength: 10, maxLength: 10 })} />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Tool name</label>
+          <input type='text' className='form-control' {...register('toolname', { required: true })} />
+        </div>
+        <div className='col-md-6'>
+          <label className='form-label'>Tool Image</label>
+          <input type='file' accept="image/*" className='form-control' {...register('toolimage', { required: true })} />
+        </div>
       </div>
-      <div className='col'>
-        <button className='btn btn-danger mt-3'>Register</button>
+      <div className='col text-center mt-3'>
+        <button className='btn btn-danger'>Register</button>
       </div>
     </form>
-  )
+  );
 }
 
-export default EquipmentForm
+export default EquipmentForm;
