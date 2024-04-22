7432468ef2e1133a1db912958685d81b9d02cd96
bharath <bharathbharath6456@gamail.com>
2024-04-22 19:32:40 +0530

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Appie from './screens/Appie.js'
 import Home from './screens/Home';
 import Login from './screens/Login';
 import Signup from './screens/Signup.js';
 import About from './screens/About.js';
 
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+// import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+// import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Pricelist from './screens/Pricelist.js';
 import RegistrationForm from './screens/RegistrationForm.js';
 import OrderSuccess from './screens/OrderSuccess.js';
